800c284c632c5b084e48aeb94d01167fb3f4a8c0
Adrian Trapletti <a.trapletti@ustermetrics.com>
2024-10-15 11:09:12 +0200

    Rename args

--- src/main/java/com/ustermetrics/clarabel4j/Matrix.java
+++ src/main/java/com/ustermetrics/clarabel4j/Matrix.java
@@ -14,28 +14,27 @@ import static java.lang.Math.toIntExact;
  * A parameter object for a
  * <a href="https://en.wikipedia.org/wiki/Sparse_matrix#Compressed_sparse_column_(CSC_or_CCS)">sparse Column Compressed Storage (CCS) matrix</a>.
  *
- * @param nzVal  data
- * @param rowVal row index. Entries within each column need to appear in order of increasing row index.
+ * @param m      number of rows
+ * @param n      number of columns
  * @param colPtr column index
- * @param nRows  number of rows
- * @param nCols  number of columns
+ * @param rowVal row index. Entries within each column need to appear in order of increasing row index.
+ * @param nzVal  data
  */
 @Builder
-public record Matrix(double @NonNull [] nzVal, long @NonNull [] rowVal, long @NonNull [] colPtr, int nRows, int nCols) {
+public record Matrix(int m, int n, long @NonNull [] colPtr, long @NonNull [] rowVal, double @NonNull [] nzVal) {
 
     public Matrix {
-        checkArgument(nRows > 0, "number of rows must be positive");
-        checkArgument(nCols > 0, "number of columns must be positive");
+        checkArgument(m > 0, "number of rows must be positive");
+        checkArgument(n > 0, "number of columns must be positive");
 
         val nnz = nzVal.length;
-        checkArgument(nnz == rowVal.length,
-                "length of data must be equal to the length of the row index");
-        checkArgument(colPtr.length == nCols + 1,
+        checkArgument(nnz == rowVal.length, "length of data must be equal to the length of the row index");
+        checkArgument(colPtr.length == n + 1,
                 "length of the column index must be equal to the number of columns plus one");
-        checkArgument(0 < nnz && nnz <= nRows * nCols,
+        checkArgument(0 < nnz && nnz <= m * n,
                 "number of non-zero entries must be greater than zero and less equal than the number of rows times " +
                         "the number of columns");
-        checkArgument(Arrays.stream(rowVal).allMatch(i -> 0 <= i && i < nRows),
+        checkArgument(Arrays.stream(rowVal).allMatch(i -> 0 <= i && i < m),
                 "entries of the row index must be greater equal zero and less than the number of rows");
         checkArgument(colPtr[0] == 0 && colPtr[colPtr.length - 1] == nnz,
                 "the first entry of the column index must be equal to zero and the last entry must be equal to the " +
