890b27dfb99ecec66ec68113edfe3ccb7543561a
Adrian Trapletti <a.trapletti@ustermetrics.com>
2025-01-03 10:56:23 +0100

    Rename

--- src/test/java/com/ustermetrics/clarabel4j/StatusTest.java
+++ src/test/java/com/ustermetrics/clarabel4j/StatusTest.java
@@ -11,7 +11,7 @@ class StatusTest {
 
     @ParameterizedTest
     @ValueSource(ints = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10})
-    void valueOfThrowsNoException(int status) {
+    void valueOfDoesNotThrow(int status) {
         assertDoesNotThrow(() -> Status.valueOf(status));
     }
 
