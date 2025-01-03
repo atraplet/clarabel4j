b181dcab962c5a90fd9b5ef06c5bf9a86463daf1
Adrian Trapletti <a.trapletti@ustermetrics.com>
2025-01-03 10:44:05 +0100

    Add test for Status class

---
+++ src/test/java/com/ustermetrics/clarabel4j/StatusTest.java
@@ -0,0 +1,25 @@
+package com.ustermetrics.clarabel4j;
+
+import lombok.val;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatusTest {
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10})
+    void valueOfThrowsNoException(int status) {
+        assertDoesNotThrow(() -> Status.valueOf(status));
+    }
+
+    @Test
+    void valueOf100ThrowsException() {
+        val exception = assertThrowsExactly(IllegalArgumentException.class, () -> Status.valueOf(100));
+
+        assertEquals("Unknown status 100", exception.getMessage());
+    }
+
+}
