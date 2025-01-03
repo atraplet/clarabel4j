5011bbd23005c2fe9e07cbb3c3ba4f39ebb6918a
Adrian Trapletti <a.trapletti@ustermetrics.com>
2025-01-03 10:46:32 +0100

    Add test for DirectSolveMethod class

---
+++ src/test/java/com/ustermetrics/clarabel4j/DirectSolveMethodTest.java
@@ -0,0 +1,25 @@
+package com.ustermetrics.clarabel4j;
+
+import lombok.val;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DirectSolveMethodTest {
+
+    @ParameterizedTest
+    @ValueSource(ints = {0})
+    void valueOfThrowsNoException(int status) {
+        assertDoesNotThrow(() -> DirectSolveMethod.valueOf(status));
+    }
+
+    @Test
+    void valueOf100ThrowsException() {
+        val exception = assertThrowsExactly(IllegalArgumentException.class, () -> DirectSolveMethod.valueOf(100));
+
+        assertEquals("Unknown direct solve method 100", exception.getMessage());
+    }
+
+}
